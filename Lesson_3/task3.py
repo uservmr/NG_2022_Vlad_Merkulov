e3f211726281ebce1f9dd71415bb21f9b559a566
uservmr <mvladyslavr@gmail.com>
2022-12-24 09:29:48 +0200

    Changed to every element

--- Lesson_3/task3.py
+++ Lesson_3/task3.py
@@ -1,4 +1,3 @@
+from collections import Counter
 string = str(input("Enter a string:"))
-letter = input("Enter a letter:")
-numberofletters = string.count(letter)
-print ("Number of letter:", numberofletters)
+print(Counter(string))
